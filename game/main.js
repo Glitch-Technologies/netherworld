1a919a2eb1f79e339b5271892e1308273a957892
greenturtle537 <support@glitchtech.top>
2024-07-19 22:52:32 +0000

    Adding some placeholders

---
+++ game/main.js
@@ -0,0 +1,29 @@
+function loadStats() {
+    const statBar = document.getElementById('statbar');
+    statBar.innerText = `
+Name: Player
+Class: Warrior
+Hit Points 10/20
+Mana Points 15/20
+Gold 100
+Experience 0/100
+Stats:
+Strength 10
+Defense 5
+Agility 5
+Intelligence 5
+Charisma 5
+Wisdom 5
+Constitution 5
+Perception 5
+Luck 5
+Status: Alive
+    `;
+}
+
+function main() {
+    loadStats();
+}
+
+//Begin execution
+main();
