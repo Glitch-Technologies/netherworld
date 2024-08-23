300da914c8ca480f22ee3f69365d75ed1fd0750c
greenturtle537 <support@glitchtech.top>
2024-08-23 10:04:05 +0000

    Fix issues with font codes

--- game/main.js
+++ game/main.js
@@ -153,9 +153,17 @@ function drawText(text, x, y, color=0) {
     const fontHeight = 8;
     const fontMap = 
 ` ☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼ !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_\`abcdefghijklmnopqrstuvwxyz{|}~`; 
+    //fontMap is for reference only. These characters will not translate through file systems properly.
+    const fontCodeMap = [
+        32,9786,9787,9829,9830,9827,9824,8226,9688,9675,9689,9794,9792,9834,9835,9788,
+        9658,9668,8597,8252,182,167,9644,8616,8593,8595,8594,8592,8735,8596,9650,9660,
+        32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,
+        58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,
+        84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,
+        108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126];
     //129 more character to add, but this is good enough for demo
     for (let i = 0; i < text.length; i++) {
-        const charIndex = fontMap.indexOf(text[i]);
+        const charIndex = fontCodeMap.indexOf(text.charCodeAt(i));
         const sx = charIndex * fontWidth;
         const sy = 0;
         ctx.drawImage(fonts[color], sx, sy, fontWidth, fontHeight, x + (i * fontWidth), y, fontWidth, fontHeight);
