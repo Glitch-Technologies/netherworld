c34f69fcce7e2b1a0c4686377884f7b9af9124f1
greenturtle537 <support@glitchtech.top>
2024-08-24 07:11:58 +0000

    Add comments

--- game/main.js
+++ game/main.js
@@ -160,8 +160,9 @@ function drawText(text, x, y, color=0) {
         32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,
         58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,
         84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,
-        108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126];
-    //129 more character to add, but this is good enough for demo
+        108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126
+    ]; //This is the IBM Code Page 437 character set. For proper charmapping, see the wikipedia page check the fontmap variable in a text editor with proper unicode support.
+    //129 more characters to add, but this is good enough for demo
     for (let i = 0; i < text.length; i++) {
         const charIndex = fontCodeMap.indexOf(text.charCodeAt(i));
         const sx = charIndex * fontWidth;
